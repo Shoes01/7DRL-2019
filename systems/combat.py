f7e3792c8e45399ab74491591459b91f65465252
Shoes01 <0jason.gilbert@gmail.com>
2019-02-16 15:12:14 -0400

    Prevent negative damage from healing the defender

--- systems/combat.py
+++ systems/combat.py
@@ -8,9 +8,14 @@ def attack(attacker, defender):
     
     damage = attacker.stats.attack - defender.stats.defense
 
-    defender.stats.hp -= damage
-
-    turn_results.append({'message': ('The {0} deals {1} damage to the {2}.'.format(attacker.base.name.capitalize(), damage, defender.base.name.capitalize()), libtcod.yellow)})
+    if damage <= 0:
+        damage = 0
+        _message = 'The {0} takes no damage!'.format(defender.base.name.capitalize())
+        _color = libtcod.light_yellow
+        turn_results.append({'message': (_message, _color)})
+    else:
+        defender.stats.hp -= damage
+        turn_results.append({'message': ('The {0} deals {1} damage to the {2}.'.format(attacker.base.name.capitalize(), damage, defender.base.name.capitalize()), libtcod.yellow)})
 
     if defender.stats.hp <= 0:
         turn_results.extend(kill(defender))
