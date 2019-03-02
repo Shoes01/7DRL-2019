89566ba96f4baaa5eab94b17eae9b033bbd3dcbe
Shoes01 <0jason.gilbert@gmail.com>
2019-03-02 18:12:00 -0400

    Clean up borders of the compare state

--- render_functions/render_compare_items.py
+++ render_functions/render_compare_items.py
@@ -54,13 +54,28 @@ def print_border(console):
     ESW = u'\u2566'
     NEW = u'\u2569'
     NESW = u'\u256c'
+    WSE_special = u'\u2564'
+    NWS_special = u'\u2567'
+    NS_special = u'\u2502'
     left_bookend = u'\u2561'
     right_bookend = u'\u255e'
 
+    # Outline of the menu
     for x in range(MAP.W):
         console.print(x, 0, EW, fg=COLORS['hud_border_fg'], bg=libtcod.black, bg_blend=libtcod.BKGND_SET)
         console.print(x, MAP.H - 1, EW, fg=COLORS['hud_border_fg'], bg=libtcod.black, bg_blend=libtcod.BKGND_SET)
     
     for y in range(MAP.H):
         console.print(0, y, NS, fg=COLORS['hud_border_fg'], bg=libtcod.black, bg_blend=libtcod.BKGND_SET)
+        console.print((MAP.W - 1) // 2, y, NS_special, fg=COLORS['hud_border_fg'], bg=libtcod.black, bg_blend=libtcod.BKGND_SET)
         console.print(MAP.W - 1, y, NS, fg=COLORS['hud_border_fg'], bg=libtcod.black, bg_blend=libtcod.BKGND_SET)
+
+    # Corners
+    console.print(0, 0, ES, fg=COLORS['hud_border_fg'], bg=libtcod.black, bg_blend=libtcod.BKGND_SET)
+    console.print(MAP.W - 1, 0, SW, fg=COLORS['hud_border_fg'], bg=libtcod.black, bg_blend=libtcod.BKGND_SET)
+    console.print(0, MAP.H - 1, NE, fg=COLORS['hud_border_fg'], bg=libtcod.black, bg_blend=libtcod.BKGND_SET)
+    console.print(MAP.W - 1, MAP.H - 1, NW, fg=COLORS['hud_border_fg'], bg=libtcod.black, bg_blend=libtcod.BKGND_SET)
+
+    # Column splitter thing
+    console.print((MAP.W - 1)//2, 0, WSE_special, fg=COLORS['hud_border_fg'], bg=libtcod.black, bg_blend=libtcod.BKGND_SET)
+    console.print((MAP.W - 1)//2, MAP.H - 1, NWS_special, fg=COLORS['hud_border_fg'], bg=libtcod.black, bg_blend=libtcod.BKGND_SET)
