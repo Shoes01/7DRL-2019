ce497475768d652056ec2aa0cb7d5f722c36f053
Shoes01 <0jason.gilbert@gmail.com>
2019-03-02 07:42:23 -0400

    Start the soul system

--- components/soul.py
+++ components/soul.py
@@ -1,30 +1,12 @@
 import numpy as np
 import random
 
+from systems.soul import generate_soul
+
 class Soul():
     def __init__ (self, eccentricity, rank):
         ' The soul is a 2x3 matrix that holds integers. The values are used to inform the stat values. '  
         self.eccentricity = eccentricity    # The greatest distance between two integers
         self.rank = rank                    # The sum total of the integers
-    
-        self.soul = np.zeros((2, 3), dtype=int, order='F')
 
-        self.generate_soul()
-    
-    def generate_soul(self):
-        attempts = 0
-        soul_attempt = np.zeros((2, 3), dtype=int, order='F')
-
-        while True:
-            with np.nditer(soul_attempt, op_flags=['readwrite']) as it:
-                for x in it:
-                    x[...] = random.randint(-self.eccentricity, self.eccentricity)
-            
-            if soul_attempt.sum() == self.rank:
-                self.soul = soul_attempt
-                return False
-                
-            elif attempts == 100:
-                return False
-            
-            attempts += 1
+        self.soul = self.generate_soul(self.eccentricity, self.rank)
---
+++ systems/soul.py
@@ -0,0 +1,22 @@
+import numpy as np
+import random
+
+def generate_soul(eccentricity, rank):
+    attempts = 0
+    soul_attempt = np.zeros((2, 3), dtype=int, order='F')
+
+    while attempts < 100:
+        with np.nditer(soul_attempt, op_flags=['readwrite']) as it:
+            for x in it:
+                x[...] = random.randint(-eccentricity, eccentricity)
+        
+        if soul_attempt.sum() == rank:
+            return soul_attempt
+
+        attempts += 1
+    
+    return np.zeros((2, 3), dtype=int, order='F')
+
+def soul_pairing(soul):
+    # This orders the numbers in the soul, and assigns them to a stat, as described by the race of the unit.
+    pass
