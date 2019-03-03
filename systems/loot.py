9e5917a42298ed46c8cc4703dd3db3cea11e61c7
Shoes01 <0jason.gilbert@gmail.com>
2019-03-03 10:16:56 -0400

    Loot scatters when dropped

--- systems/loot.py
+++ systems/loot.py
@@ -1,32 +1,36 @@
+import random
+
 from systems.factory import create_item
 
 def drop_loot(entity, entities):
     turn_results = []
     
+    directions = [(1, -1), (1, 0), (1, 1), (0, -1), (0, 1), (-1, -1), (-1, 0), (-1, 1)]
+
     x, y = entity.pos.x, entity.pos.y
-    
+
     item = create_item('sword')
-
-    item.pos.x, item.pos.y = x, y
-
+    direction = random.choice(directions)
+    directions.remove(direction)    
+    item.pos.x, item.pos.y = x + direction[0], y + direction[0]
     entities.append(item)
 
     item = create_item('boots')
-
-    item.pos.x, item.pos.y = x, y
-
+    direction = random.choice(directions)
+    directions.remove(direction)    
+    item.pos.x, item.pos.y = x + direction[0], y + direction[0]
     entities.append(item)
 
     item = create_item('chainmail')
-
-    item.pos.x, item.pos.y = x, y
-
+    direction = random.choice(directions)
+    directions.remove(direction)    
+    item.pos.x, item.pos.y = x + direction[0], y + direction[0]
     entities.append(item)
 
     item = create_item('shield')
-
-    item.pos.x, item.pos.y = x, y
-
+    direction = random.choice(directions)
+    directions.remove(direction)    
+    item.pos.x, item.pos.y = x + direction[0], y + direction[0]
     entities.append(item)
 
     return turn_results
