95005753ee5a320595922c6be3b9957228757b98
Shoes01 <0jason.gilbert@gmail.com>
2019-03-04 13:08:29 -0400

    Update input handler for new state

--- input_handlers.py
+++ input_handlers.py
@@ -13,6 +13,8 @@ def handle_keys(game_state_machine, key):
         return handle_targeting_state_keys(key)
     elif _game_state == 'CompareItems':
         return handle_compare_items_keys(key)
+    elif _game_state == 'ConsumeSoul':
+        return handle_consume_soul_keys(key)
     else:
         return handle_general_keys(key)
 
@@ -145,9 +147,35 @@ def handle_generic_movement_keys(key):
     return {}
 
 def handle_compare_items_keys(key):
-    if key.vk == libtcod.KEY_SPACE:
+    if key.vk == libtcod.KEY_KP5:
         return {'confirm': True}
-    elif handle_generic_movement_keys(key):
+    elif key.vk == libtcod.KEY_SPACE:
+        return {'confirm': False}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        # Alt+Enter: toggle full screen
+        return {'fullscreen': True}
+    elif key.vk == libtcod.KEY_ESCAPE:
+        # Exit the game
+        return {'exit': True}
+    
+    return {}
+
+def handle_consume_soul_keys(key):
+    # Rotate the soul.
+    if key.vk == libtcod.KEY_UP or key_char == 'k' or key.vk == libtcod.KEY_KP8:
+        return {'move': (0, -1)}
+    elif key.vk == libtcod.KEY_DOWN or key_char == 'j' or key.vk == libtcod.KEY_KP2:
+        return {'move': (0, 1)}
+    elif key.vk == libtcod.KEY_LEFT or key_char == 'h' or key.vk == libtcod.KEY_KP4:
+        return {'move': (-1, 0)}
+    elif key.vk == libtcod.KEY_RIGHT or key_char == 'l' or key.vk == libtcod.KEY_KP6:
+        return {'move': (1, 0)}
+
+    # Confirmation keys.
+    if key.vk == libtcod.KEY_KP5:
+        return {'confirm': True}
+    elif key.vk == libtcod.KEY_SPACE:
         return {'confirm': False}
 
     if key.vk == libtcod.KEY_ENTER and key.lalt:
