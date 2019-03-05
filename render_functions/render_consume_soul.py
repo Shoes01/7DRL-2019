43a6dcf1ddde6c3a0c17f839e423a7e3d457eb90
Shoes01 <0jason.gilbert@gmail.com>
2019-03-05 09:04:22 -0400

    Improve soul consumption screen

--- render_functions/render_consume_soul.py
+++ render_functions/render_consume_soul.py
@@ -2,7 +2,7 @@ import tcod as libtcod
 
 from components.base import RenderOrder
 from game import COLORS, MAP
-from systems.stats import get_stats
+from systems.stats import get_ordered_soul, get_stats
 
 def render_consume_soul(consoles, entities, player):
     console = consoles['map']
@@ -27,6 +27,32 @@ def print_text(console, entities, player):
         if entity.base.render_order == RenderOrder.SOUL and entity.pos.x == player.pos.x and entity.pos.y == player.pos.y:
             new_soul = entity.soul.soul
 
+    console.print(3, 3, 'Proposed soul consumption:')
+
+    console.print(4, 5, str(soul))
+    console.print(17, 5, '+')
+    console.print(19, 5, str(new_soul))
+    console.print(32, 5, '=')
+    console.print(34, 5, str(soul + new_soul))
+    
+    console.print(3, 8, 'Stat calculation')
+    console.print(4, 9, 'Race bonus + soul number = stat total')
+    console.print(4, 10, 'Your job determines the order of the stats.')
+    console.print(4, 11, 'HP is x4. If the total is less than 10, it is 10 instead.')
+
+    new_stats = get_ordered_soul(None, player.job, new_soul + soul)
+    total_stats = get_stats(None, player.job, player.race, new_soul + soul)
+
+    order = player.job.value['stats']
+    y = 0
+    for stat in order:
+        _string = stat + ' : ' + str(player.race.value['bonus']) +  ' + ' + '{:>3}'.format(str(new_stats[stat])) +  ' = ' + '{:>3}'.format(str(total_stats[stat]))
+        if stat == 'HP':
+            _string = stat + '  : ' + str(player.race.value['bonus']) +  ' + ' +'{:>3}'.format(str(new_stats[stat] // 4)) +  ' = ' + '{:>3}'.format(str(total_stats[stat] // 4)) + ' x4' + ' = ' + str(total_stats[stat]
+        console.print(4, 13 + y, _string)
+        y += 1
+
+    """
     # Print left column
     ### Print the player's soul
     string_1 = 'Your soul:\n\n' + str(soul)
@@ -34,6 +60,7 @@ def print_text(console, entities, player):
 
     ### Print stats
     stats = get_stats(player)
+    new_stats = get_stats(None, player.job, player.race, new_soul + soul)
     HP = 'HP :{:>3}/{:>3}'.format(str(player.health.points), str(player.health.max))
     SPD = 'SPD:{:>2}'.format(str(stats.get('SPD')))
     ATK = 'ATK:{:>2}'.format(str(stats.get('ATK')))
@@ -87,6 +114,7 @@ def print_text(console, entities, player):
         
         console.print(3 + MAP.W // 2 + 14, 7 + y, '{:>3}'.format(_string), fg=_color)
         y += 1    
+    """
 
 def print_border(console):
     # Unicode cheat sheet.
@@ -114,7 +142,6 @@ def print_border(console):
     
     for y in range(MAP.H):
         console.print(0, y, NS, fg=COLORS['hud_border_fg'], bg=libtcod.black, bg_blend=libtcod.BKGND_SET)
-        console.print((MAP.W - 1) // 2, y, NS_special, fg=COLORS['hud_border_fg'], bg=libtcod.black, bg_blend=libtcod.BKGND_SET)
         console.print(MAP.W - 1, y, NS, fg=COLORS['hud_border_fg'], bg=libtcod.black, bg_blend=libtcod.BKGND_SET)
 
     # Corners
--- systems/stats.py
+++ systems/stats.py
@@ -21,3 +21,23 @@ def get_stats(entity, job=None, race=None, soul=None):
         stats['HP'] = stats['HP'] * 4
 
         return stats
+
+def get_ordered_soul(entity, job=None, soul=None):
+    if entity is not None:
+        soul = entity.soul.soul.copy()
+        soul = soul.reshape(6).tolist()
+        soul.sort(reverse=True)
+        
+        stats = dict(zip(entity.job.value.get('stats'), soul))
+        stats['HP'] = stats['HP'] * 4
+
+        return stats
+    else:
+        soul = soul.copy()
+        soul = soul.reshape(6).tolist()
+        soul.sort(reverse=True)
+
+        stats = dict(zip(job.value.get('stats'), soul))
+        stats['HP'] = stats['HP'] * 4
+
+        return stats
