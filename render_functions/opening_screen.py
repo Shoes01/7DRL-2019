ecaea543f8855f308e7cfd0e0ea86924fe1580a4
Shoes01 <0jason.gilbert@gmail.com>
2019-03-08 20:05:20 -0400

    Update opening screen

--- render_functions/opening_screen.py
+++ render_functions/opening_screen.py
@@ -12,7 +12,7 @@ def render_opening_screen(consoles):
     console.print(3, 3, 'You must defeat the demon prince of this dungeon! Go on.')
 
     console.print(3, 5, 'Additionally, here is some information on how to play.')
-    console.print(3, 6, 'Use the numpad to move. Use Spacebar to equip items from the ground, or to inspect souls before consuming them. Press 5 on the numpad to confirm choice.')
+    console.print(3, 6, 'Use the numpad to move. Use Spacebar to equip items from the ground, or to inspect souls before consuming them. Press 5 on the numpad or enter to confirm choice.')
     console.print(3, 7, 'QWEASD keys use skills associated with the item you picked up. I think that is all you need!!')
 
     console.print(3, 9, 'Press any of these actions to begin.')
